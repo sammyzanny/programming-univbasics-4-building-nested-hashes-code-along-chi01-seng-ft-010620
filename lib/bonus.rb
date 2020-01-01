f55063feee6f74936ba95ac33fe56edff4879621
Samuel Zandi <samuelczandi@gmail.com>
2020-01-01 21:58:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,7 @@ def bonus
    }
   }
 
-  #code your solution here:
-
-  
+  #code your 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
